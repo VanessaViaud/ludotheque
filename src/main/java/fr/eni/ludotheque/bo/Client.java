db626b0c64dea2eb149c579168f04df55f2ed2fc
VanessaViaud <vanessa.viaud@gmail.com>
2025-10-28 16:45:58 +0100

    feature/add_dal

--- src/main/java/fr/eni/ludotheque/bo/Client.java
+++ src/main/java/fr/eni/ludotheque/bo/Client.java
@@ -1,5 +1,6 @@
 package fr.eni.ludotheque.bo;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
@@ -7,12 +8,26 @@ import java.util.UUID;
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "CLIENTS")
 public class Client {
 
-    @EqualsAndHashCode.Exclude private Integer clientNumber;
+    @EqualsAndHashCode.Exclude
+    @Id
+    @GeneratedValue
+    private Integer clientNumber;
+
+    @Column(nullable = false, length = 100)
     @NonNull private String lastname;
+
+    @Column(nullable = false, length = 100)
     @NonNull private String firstname;
+
+    @Column(nullable = false, length = 100, unique = true)
     @NonNull private String email;
-    @NonNull private Address address;
+
+   // @NonNull private Address address;
+
+    @Column(length = 15)
     private String phoneNumber;
 }
